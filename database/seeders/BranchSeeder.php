666dcc5255b747fb888d40d894bdba24bcf5b694
GKiller7 <batrbatyrow3@gmail.com>
2024-07-01 17:50:59 +0500

    1.5.0

---
+++ database/seeders/BranchSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Branch;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BranchSeeder extends Seeder
+{
+    public function run()
+    {
+        $branchs = [
+            'Beginner',
+            'Elementary',
+            'Upper-Intermediate',
+            'Advanced',
+            'Comp-9',
+            'Comp-10',
+            'Comp-11',
+            'Math-1',
+            'Math-2',
+        ];
+
+        foreach ($branchs as $branch) {
+            Branch::create([
+                'name' => $branch,
+            ]);
+        }
+    }
+}
