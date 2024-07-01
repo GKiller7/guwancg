3d082b59f892536473670e7805ecfec5f8656b39
GKiller7 <batrbatyrow3@gmail.com>
2024-07-01 17:45:55 +0500

    1.3.0

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'first_name',
+        'last_name',
+        'phone',
+        'gender',
+        'created_at',
+        'update_at',
+    ];
+
+    public function registrations(){
+        return $this->hasMany(Registration::class);
+    }
+}
