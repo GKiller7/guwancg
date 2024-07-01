0796bc27c3d734bb58a41b0e38cb5e534bec80d6
GKiller7 <batrbatyrow3@gmail.com>
2024-07-01 17:42:35 +0500

    1.2.0

---
+++ database/seeders/ClassroomSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Classroom;
+use App\Models\Location;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassroomSeeder extends Seeder
+{
+    public function run()
+    {
+        $classrooms = [
+            ['name' => 'A1'],
+            ['name' => 'B2'],
+            ['name' => 'C3'],
+            ['name' => 'D5'],
+            ['name' => 'D4'],
+            ['name' => 'A5'],
+            ['name' => 'A7'],
+            ['name' => 'B6'],
+            ['name' => 'B8'],
+            ['name' => 'A4'],
+        ];
+
+        foreach ($classrooms as $classroomData) {
+            Classroom::create([
+                'name' => $classroomData['name'],
+                'location_id' => Location::inRandomOrder()->first()->id,
+            ]);
+        }
+    }
+}
