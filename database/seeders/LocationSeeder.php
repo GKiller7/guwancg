350728ed8c7f67a9165ae7c2bf8ab1d12d1ba4cf
GKiller7 <batrbatyrow3@gmail.com>
2024-07-01 17:32:35 +0500

    1.0.0

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Location;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LocationSeeder extends Seeder
+{
+    public function run()
+    {
+        $locations = [
+            'Anew',
+            'Mir-1',
+            'Mir-2',
+        ];
+
+        foreach ($locations as $location) {
+            Location::create([
+                'name' => $location,
+            ]);
+        }
+    }
+}
