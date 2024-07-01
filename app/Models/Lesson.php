7c2bfbab16626e274df39b93ce38adda2fb413e0
GKiller7 <batrbatyrow3@gmail.com>
2024-07-01 17:55:15 +0500

    1.6.0

---
+++ app/Models/Lesson.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'classroom_id',
+        'branch_id',
+        'teacher_id',
+        'created_id',
+        'updated_id',
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    // many to one
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function registrations()
+    {
+        return $this->belongsTo(Registration::class);
+    }
+}
